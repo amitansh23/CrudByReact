f30dd28976589439446201d1d7959fa61e33c40a
amitansh <amitanshchaurasiya@gmail.com>
2025-02-19 16:19:44 +0530

    19 commit

--- src/components/getuser/User.jsx
+++ src/components/getuser/User.jsx
@@ -1,39 +1,62 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState, useCallback } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./User.css";
 import axios from "axios";
 import toast from "react-hot-toast";
 import Swal from "sweetalert2";
+import { DateTime } from "luxon";
 
 const User = () => {
+
+  const navigate = useNavigate()
   const [user, setUser] = useState(null);
   const [users, setUsers] = useState([]);
   const [search, setSearch] = useState("");
 
   const [count, setCount] = useState(5);
   const [total, setTotal] = useState(0);
+  const [sortField, setSortField] = useState("Created_at");
+  const [sortOrder, setSortOrder] = useState("desc");
+  const [selectedRole, setSelectedRole] = useState("");
 
-  const fetchData = async (limit, query) => {
+  const fetchData =useCallback(async (limit, query, field, order, role) => {
     // const token = localStorage.getItem("token");
 
     // const res = await axios.get('http://localhost:8000/api/getall',{ headers : {
     //   'auth': token}})
     // setUsers(res.data)
+try {
+  const res = await axios.get(
+    `http://localhost:8000/api/getlimiteddata?count=${limit}&search=${query}&sortField=${field}&sortOrder=${order}&role=${role}`,
+    {
+      // headers: {
+      //   auth: token,
+      // },
+      withCredentials: true
+    }
 
-    const res = await axios.get(
-      `http://localhost:8000/api/getlimiteddata?count=${limit}&search=${query}`,
-      {
-        // headers: {
-        //   auth: token,
-        // },
-        withCredentials: true
-      }
+  );
+  setUsers(res?.data?.userData);
+  setTotal(res?.data?.total);
+  
 
-    );
-    setUsers(res?.data?.userData);
-    setTotal(res?.data?.total);
-    console.log(res?.data?.userData);
-  };
+  
+} catch (error) {
+  console.error("Error fetching data:", error);
+  navigate('/login')
+  
+  
+}},[navigate]);
+  
+
+  const toggleSort = (field) => {
+    const newOrder = sortField === field && sortOrder === "asc" ? "desc" : "asc";
+    setSortField(field);
+    setSortOrder(newOrder);
+    // fetchData(count, search, field, newOrder);
+  }
+
+
 
   const handlenext = () => {
     setCount(count + 5);
@@ -45,13 +68,18 @@ const User = () => {
     fetchData(5, query);
   };
 
+  const handleRoleChange = (e) => {
+    setSelectedRole(e.target.value);
+  }
+
   useEffect(() => {
     const storedUser = localStorage.getItem("userData");
     if (storedUser) {
       setUser(JSON.parse(storedUser));
+     
     }
-    fetchData(count, search);
-  }, [count, search]);
+    fetchData(count, search, sortField, sortOrder, selectedRole);
+  }, [count, search, sortField, sortOrder, selectedRole, fetchData]);
 
   const DELETE = async (userId) => {
     Swal.fire({
@@ -110,22 +138,41 @@ const User = () => {
       <Link to={"/restore"} className="addButton">
         RESTORE
       </Link>
-
+      
       <input
         type="text"
-        placeholder="Search items..."
+        placeholder="  Search items.."
         value={search}
         onChange={handleSearch}
       />
+     
+      <div className="dropbox">
+      <select onChange= {handleRoleChange} value={selectedRole}>
+        <option value="">All</option>
+        <option value="0">SuperAdmin</option>
+        <option value="1">Admin</option>
+        <option value="2">User</option>
+      </select>
+      </div>
 
       <table border={1} cellPadding={15} cellSpacing={8}>
         <thead>
           <tr>
             <th>S.NO.</th>
             <th>Name</th>
-            <th>Email</th>
+            <th>Email{" "}<span onClick={()=>{toggleSort("email")}} style={{cursor:"pointer"}}>
+            {sortField === "email" ? (sortOrder === "asc" ? "▲" :"▼"): "↕" }
+            </span>
+            
+            </th>
             <th>Role</th>
             <th>CreatedBy</th>
+            <th>CreatedAt{" "}<span onClick={()=>{toggleSort("Created_at")}} style={{cursor:"pointer"}}>
+            {sortField === "Created_at" ? (sortOrder === "asc" ? "▲" :"▼"): "↕" }
+            </span>
+            </th>
+            <th>UpdatedAt</th>
+
             <th>Actions</th>
           </tr>
         </thead>
@@ -145,6 +192,9 @@ const User = () => {
                   : "User"}
               </td>
               <td>{user?.CreatedBy?.fname? user.CreatedBy.fname : "NA"}</td>
+
+              <td>{DateTime.fromISO(user.Created_at).toFormat("dd LLL yyyy, hh:mm a")}</td>
+              <td>{DateTime.fromISO(user.Updated_at).toFormat("dd LLL yyyy, hh:mm a")}</td>
 
 
               <td className="actionButton">
@@ -171,7 +221,7 @@ const User = () => {
           ))}
         </tbody>
       </table>
-      <button className="addButton" onClick={handlenext}>
+      <button className="addButton" onClick={handlenext} >
         Next
       </button>
     </div>
